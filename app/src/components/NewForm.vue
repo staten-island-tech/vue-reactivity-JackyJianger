c7a009c87b398c4b35da088a4960ae5d554f9627
JackyJianger <158211567+JackyJianger@users.noreply.github.com>
2025-01-28 12:44:29 -0500

    a

---
+++ app/src/components/NewForm.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <form name="login-form" action="click" @submit.prevent="login(user)">
+        <label for="username">Enter Username</label>
+        <input type="text" v-model="user.username"/>
+        <label for="password">Enter Password</label>
+        <input type="password" v-model="user.password"/>
+        <button type="submit">Submit</button>
+        </form>
+    </div>
+</template>
+
+<script setup>
+import { reactive } from 'vue';
+const user = reactive({username:'', password: ''})
+function login(user){
+    console.log(user)
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ app/src/views/NewForm.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <NewForm></NewForm>
+    </div>
+</template>
+
+<script setup>
+import NewForm from '@/components/NewForm.vue';
+</script>
+
+<style scoped>
+
+</style>
